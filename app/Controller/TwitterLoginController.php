7899702980a6b0b89c1c07630e402f5768db7a09
gurimmer <s.reianai@gmail.com>
2013-11-03 17:38:42 +0900

    change sql

--- app/Controller/TwitterLoginController.php
+++ app/Controller/TwitterLoginController.php
@@ -91,8 +91,11 @@ class TwitterLoginController extends LoginController {
             $this->log(get_object_vars($me), "debug");
 
             $this->loadModel("User");
-            $twUser = $this->User->find("all", array("conditions" => array("User.tw_user_id"=>$me->id_str)));
+
+            $twUser = $this->User->find("first", array("conditions" => array("User.tw_user_id"=>$me->id_str)));
+            // $twUser = $this->User->find("all", array("conditions" => array("User.tw_user_id"=>$me->id_str)));
             $this->log(get_object_vars($twUser), "debug");
+
             if (!$twUser) {
                 $twUser = new User();
                 $twUser->tw_user_id = $me->id_str;
