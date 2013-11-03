dfefa2da59f9aa9ed13f340d47260db66eabbafd
gurimmer <s.reianai@gmail.com>
2013-11-03 16:14:12 +0900

    update login

--- app/Controller/TwitterLoginController.php
+++ app/Controller/TwitterLoginController.php
@@ -40,8 +40,50 @@ class TwitterLoginController extends LoginController {
             // redirect to auth website
             $auth_url = $cb->oauth_authorize();
             header('Location: ' . $auth_url);
-            die();
+        } elseif (isset($_GET['oauth_verifier']) && isset($_SESSION['oauth_verify'])) {
 
+            Codebird::setConsumerKey(CONSUMER_KEY, CONSUMER_SECRET);
+            $cb = Codebird::getInstance();
+
+            // verify the token
+            $cb->setToken($_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
+            unset($_SESSION['oauth_verify']);
+
+            // get the access token
+            $reply = $cb->oauth_accessToken(array(
+                'oauth_verifier' => $_GET['oauth_verifier']
+            ));
+
+            // store the token (which is different from the request token!)
+            $_SESSION['oauth_token'] = $reply->oauth_token;
+            $_SESSION['oauth_token_secret'] = $reply->oauth_token_secret;
+
+            $cb->setToken($reply->oauth_token, $reply->oauth_token_secret);
+
+            $me = $cb->account_verifyCredentials();
+            $this->log(get_object_vars($me), "debug");
+            //var_dump($me);
+
+            $twUser = $this->User->find("all", array("conditions" => array("User.tw_user_id"=>$me->id_str)));
+            if (!$twUser) {
+                $twUser = new User();
+                $twUser->tw_user_id = $me->id_str;
+                $twUser->tw_screen_name = $me->screen_name;
+                $twUser->tw_access_token = $reply->oauth_token;
+                $twUser->tw_access_token_secret = $reply->oauth_token_secret;
+                if ($this->User->save($twUser)) {
+                    // セッションハイジャック対策
+//                    session_regenerate_id(true);
+                    $twUser = $this->User->findById($this->User->id);
+                    $this->Session->write("Users.me", $twUser);
+                }
+            } else {
+                // セッションハイジャック対策
+//                session_regenerate_id(true);
+                $this->Session->write("Users.me", $twUser);
+
+            }
+            return $this->redirect(array("controller"=>"todos", "action"=>"index"));
         }
 
         $cb->setToken($_SESSION['oauth_token'], $_SESSION['oauth_token_secret']);
