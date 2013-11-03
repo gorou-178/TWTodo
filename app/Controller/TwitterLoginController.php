bbed76daf18474f915ea0697149b99ce4a6d033c
gurimmer <s.reianai@gmail.com>
2013-11-03 17:41:22 +0900

    bug fix

--- app/Controller/TwitterLoginController.php
+++ app/Controller/TwitterLoginController.php
@@ -91,8 +91,7 @@ class TwitterLoginController extends LoginController {
             $this->log(get_object_vars($me), "debug");
 
             $this->loadModel("User");
-
-            $twUser = $this->User->find("first", array("conditions" => array("User.tw_user_id"=>$me->id_str)));
+            $twUser = $this->User->find("first", array("conditions" => array("User.tw_user_id"=>$me["id_str"])));
             // $twUser = $this->User->find("all", array("conditions" => array("User.tw_user_id"=>$me->id_str)));
             $this->log(get_object_vars($twUser), "debug");
 
